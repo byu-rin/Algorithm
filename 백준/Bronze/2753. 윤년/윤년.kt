b693581c5c843a2ce676d4c36582704f2b7f4506
Lee do yeon <130144220+byu-rin@users.noreply.github.com>
2024-10-19 15:59:30 +0900

    [Bronze V] Title: 윤년, Time: 72 ms, Memory: 12380 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val year = readln().toInt()
+
+    if(year % 4 == 0 && year % 100 != 0) {
+    println(1) 
+    } else if(year % 4 == 0 && year % 400 == 0) {
+        println(1)
+    } else {
+        println(0)
+    }
+}
