df98bc1e4a76ece45660e8d2d4b716273672330c
Lee do yeon <130144220+byu-rin@users.noreply.github.com>
2024-10-21 22:10:13 +0900

    [Bronze III] Title: 알람 시계, Time: 112 ms, Memory: 21256 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.kt
+++ 백준/Bronze/2884. 알람 시계/알람 시계.kt
@@ -1,16 +1,14 @@
 fun main() {
     var (h, m) = readln().split(" ").map { it.toInt() }
-
     var alarm = m - 45
-
+    
     if (alarm < 0) {
         alarm += 60
         if (h == 0) {
-            h = 23  // h가 0이면 23으로
+            h = 23
         } else {
-            h -= 1  // h가 0이 아니면 1을 뺌
+            h -= 1
         }
     }
-    
     println("$h $alarm")
 }
