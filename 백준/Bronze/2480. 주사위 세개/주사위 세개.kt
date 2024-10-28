b069ff3059c112a144c5309c22b10c8bf4d7688a
Lee do yeon <130144220+byu-rin@users.noreply.github.com>
2024-10-29 01:08:55 +0900

    [Bronze IV] Title: 주사위 세개, Time: 120 ms, Memory: 20904 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.kt
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.kt
@@ -1,11 +1,18 @@
 fun main() {
-    var (first, second, third) = readln().split(" ").map { it.toInt() }
-    
-    val result = when {
-        first == second && second == third ->  10000 + first * 1000
-        first == second || first == third -> 1000 + first * 100
-        second == third -> 1000 + second * 100
-        else -> maxOf(first, second, third) * 100
+    val (first, second, third) = readln().split(" ").map { it.toInt() }
+
+    if (first == second && second == third) {
+        println(10000 + first * 1000)
+    } else if (first == second || first == third) {
+        println(1000 + first * 100)
+    } else if (second == third) {
+        println(1000 + second * 100)
+    } else {
+        val max = if (first > second) {
+            if (first > third) first else third
+        } else {
+            if (second > third) second else third
+        }
+        println(max * 100)
     }
-    println(result)
 }
