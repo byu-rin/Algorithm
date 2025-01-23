f828c49c95c9995e3e078de13c87c9fa0f0d6945
Lee do yeon <130144220+byu-rin@users.noreply.github.com>
2025-01-23 14:23:51 +0900

    [Bronze V] Title: A+B - 5, Time: 120 ms, Memory: 20624 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.kt
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.kt
@@ -1,12 +1,15 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import kotlin.collections.component1
+import kotlin.collections.component2
+import kotlin.collections.map
+
 fun main() {
-    val sum = mutableListOf<Int>()
-    do {
-        val (a, b) = readln().split(" ").map { it.toInt() }
+    val br = BufferedReader(InputStreamReader(System.`in`))
 
+    while (true) {
+        val (a, b) = br.readLine().split(' ').map { it.toInt() }
         if (a == 0 && b == 0) break
-
-        sum.add(a + b)
-    } while (true)
-
-    println(sum.joinToString("\n"))
+        println(a + b)
+    }
 }
